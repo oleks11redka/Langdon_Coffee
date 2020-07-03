bc4da56a61d9b25a3a7df154c69e472eec3e8820
marynasiedina <marynasiedina@gmail.com>
2020-07-03 14:59:03 +0300

    links

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import NavBar from "../pages/NavBar/NavBar";
+import NavBar from "../elements/NavBar/NavBar";
 import Home from "../pages/Home/Home";
 import Origins from "../pages/Origins/Origins";
 import ContactUs from "../pages/ContactUs/ContactUs";
@@ -18,7 +18,7 @@ export default class App extends Component {
   render() {
     return (
       <Router>
-        {/* <NavBar /> */}
+        <NavBar />
 
         {/* Пример кнопки которая переходит уже по ссылке */}
         {/* <GeneralButton exact link='/' buttonName='Origins' /> */}
