88509b615459911e07285273de5fdbda1cdc36cb
oleks11redka <redka.aleksey@gmail.com>
2020-07-03 13:38:57 +0300

    OurOffering element added

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import MainHeader from '../../elements/MainHeader/MainHeader'
 import WhoWeAre from '../../elements/WhoWeAre/WhoWeAre'
+import OurOffering from '../../elements/OurOffering/OurOffering'
 
 import './Home.scss'
 
@@ -10,6 +11,7 @@ export default class Home extends Component {
         <div className='Home'>
             <MainHeader />
             <WhoWeAre />
+            <OurOffering />
         </div>
       )
     }
