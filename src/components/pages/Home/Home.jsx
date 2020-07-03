6af8b980177b75747ed8583e5dfbd1395f8ca2ff
oleks11redka <redka.aleksey@gmail.com>
2020-07-03 13:47:26 +0300

    ClassNames updated

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -8,7 +8,7 @@ import './Home.scss'
 export default class Home extends Component {
     render() {
       return (
-        <div className='Home'>
+        <div className='home'>
             <MainHeader />
             <WhoWeAre />
             <OurOffering />
