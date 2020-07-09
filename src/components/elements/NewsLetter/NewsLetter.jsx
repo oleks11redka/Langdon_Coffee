0de7628d624b28a8335e5d3a1cebc0b2eb64e757
bredillya123 <bredillya@gmail.com>
2020-07-09 20:41:33 +0300

    newsletter + adap

--- src/components/elements/NewsLetter/NewsLetter.jsx
+++ src/components/elements/NewsLetter/NewsLetter.jsx
@@ -5,11 +5,14 @@ import "./NewsLetter.scss";
 export default class NewsLetter extends Component {
   render() {
     return (
-        <form>
-          <label for="email">SIGN UP FOR OUR NEWSLETTER</label>
-          <input type="email" id="email" name="email" placeholder="Your email"></input>
-          <input type="submit" value="Subscribe" />
-        </form>
+      <form className='signUp'>
+        <label className='signUp__title' for="email">SIGN UP FOR OUR NEWSLETTER</label>
+        <div className="signUp__box">
+          <input className='signUp__input' type="email" id="email" name="email" placeholder="Your email"></input>
+          <input className='signUp__button' type="submit" value="Subscribe" />
+        </div>
+
+      </form>
     );
   }
 }
