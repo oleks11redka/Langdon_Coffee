cd03b8c457fb6dd8702497d70f4a7097576d8edc
bredillya123 <bredillya@gmail.com>
2020-07-11 01:52:02 +0300

    OurCredentials css

--- src/components/elements/OurCredentials/OurCredentials.jsx
+++ src/components/elements/OurCredentials/OurCredentials.jsx
@@ -4,7 +4,16 @@ import './OurCredentials.scss'
 export default class OurCredentials extends Component {
   render() {
     return (
-      <h1>Delay tut</h1>
+      <div className="ourCredentials">
+        <h1 className="ourCredentials__title">Our Credentials</h1>
+        <div className="ourCredentials__logos">
+          <img className='oruCredentials__logo' src={require("./OurCredentialsPictures/blue-ACO-logo-1.svg")} alt="logo ACO"/>
+          <img className='oruCredentials__logo' src={require("./OurCredentialsPictures/logo.svg")} alt="logo specialety"/>
+          <img className='oruCredentials__logo' src={require("./OurCredentialsPictures/Fairtrade_Logo.svg")} alt="logo fairtrade"/>
+          <img className='oruCredentials__logo' src={require("./OurCredentialsPictures/acta.svg")} alt="logo acta"/>
+          <img className='oruCredentials__logo' src={require("./OurCredentialsPictures/seal_green_web.svg")} alt="logo frog"/>
+        </div>
+      </div>
     )
   }
 }
