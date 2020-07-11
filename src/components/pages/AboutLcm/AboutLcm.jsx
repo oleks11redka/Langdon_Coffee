49aaa4ad36b6df19442f0d8a6e36454a62f7fc43
marynasiedina <marynasiedina@gmail.com>
2020-07-11 23:10:02 +0300

    new link to aboutUs

--- src/components/pages/AboutLcm/AboutLcm.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-export default class AboutLcm extends Component {
-  render() {
-    return (
-      <h1>Hello coocoo</h1>
-    )
-  }
-}
---
+++ src/components/pages/AboutUs/AboutUs.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import './AboutUs.scss'
+
+export default class AboutUs extends Component {
+  render() {
+    return (
+      <h1 className="titleAbout">Hello coocoo</h1>
+    )
+  }
+}
