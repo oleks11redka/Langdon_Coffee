30e2175700b921f51f575f302985025861eb6b2f
oleks11redka <redka.aleksey@gmail.com>
2020-07-13 02:50:16 +0300

    NewsLetters element styling improvements

--- src/components/elements/NewsLetter/NewsLetter.jsx
+++ src/components/elements/NewsLetter/NewsLetter.jsx
@@ -11,7 +11,6 @@ export default class NewsLetter extends Component {
           <input className='signUp__input' type="email" id="email" name="email" placeholder="Your email"></input>
           <input className='signUp__button' type="submit" value="Subscribe" />
         </div>
-
       </form>
     );
   }
