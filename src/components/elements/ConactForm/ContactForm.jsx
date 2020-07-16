0a5ea95537889eeb9cb40100ad049b4a9a243da3
marynasiedina <marynasiedina@gmail.com>
2020-07-16 03:36:47 +0300

    added google form

--- src/components/elements/ConactForm/ContactForm.jsx
+++ src/components/elements/ConactForm/ContactForm.jsx
@@ -3,35 +3,47 @@ import React, { Component } from 'react'
 import './ContactForm.scss'
 
 export default class ConactForm extends Component {
+  state = {
+    submitted: false
+  }
+
 
   render() {
+
     let { formTitle, text } = this.props
 
     return (
       <div className='contactForm'>
         <h2 className='contactForm__title'>{formTitle}</h2>
         {text ? <p className='contactForm__text'>To place your coffee bean order, please contact your local Langdon Coffee Merchant representative who will talk you through order quantities, despatch and delivery:</p> : null}
-        <form className='contactForm__form' action="">
-          <input required placeholder="Name *" className='contactForm__text-input' type="text" name="name" id="name" />
-          <input required placeholder="Surname *" className='contactForm__text-input' type="text" name="surname" id="surname" />
-          <input required placeholder="Company *" className='contactForm__text-input' type="text" name="company" id="company" />
-          <input required placeholder="Email *" className='contactForm__text-input' type="email" name="email" id="email" />
-          <select required className='contactForm__selection' name="region" id="region">
+
+        {/* 
+        <iframe name="hidden_iframe" id="hidden_iframe" style="display:none;"
+          onload={this.state.submitted}></iframe> */}
+
+        <form className='contactForm__form' action="https://docs.google.com/forms/u/0/d/e/1FAIpQLSf_z7vciaBDovUQB2duCbn-L-cpmTw7eC71AX5f7yFL0OFb3g/formResponse" method="post" target="hidden_iframe">
+
+
+          <input required placeholder="Name *" className='contactForm__text-input' type="text" name="entry.77905590" id="name" />
+          <input required placeholder="Surname *" className='contactForm__text-input' type="text" name="entry.549195138" id="surname" />
+          <input required placeholder="Company *" className='contactForm__text-input' type="text" name="entry.1379143492" id="company" />
+          <input required placeholder="Email *" className='contactForm__text-input' type="email" name="entry.1422137492" id="email" />
+          <select required className='contactForm__selection' name="entry.874848661" id="region">
             <option selected disabled value>Region *</option>
             <option value="Australia">Australia</option>
             <option value="New Zealand">New Zealand</option>
             <option value="United Kingdom">United Kingdom</option>
             <option value="Other">Other</option>
           </select>
-          <input placeholder="Phone" className='contactForm__text-input' type="number" name="phoneNumber" id="phoneNumber" />
-          <select required className='contactForm__selection' name="interest" id="interest">
+          <input placeholder="Phone" className='contactForm__text-input' type="number" name="entry.618806517" id="phoneNumber" />
+          <select required className='contactForm__selection' name="entry.63549061" id="interest">
             <option selected disabled value>I am interested in *</option>
             <option value="Sampling">Requesting a sample</option>
             <option value="Cupping">Requesting a cupping</option>
             <option value="Ordering beans">Ordering coffee beans</option>
             <option value="Other">Other (please specify below)</option>
           </select>
-          <input placeholder="Your message" className='contactForm__text-input contactForm__text-input-massage' type="text" name="message" id="message" />
+          <input placeholder="Your message" className='contactForm__text-input contactForm__text-input-massage' type="text" name="entry.1892682850" id="message" />
           <div className="contactForm__checkbox-block">
             <input required className='contactForm__checkbox' type="checkbox" name="policy" id="policy" />
             <label className='contactForm__policy' htmlFor="policy">I agree to <a className='contactForm__info-lcm' href="https://www.langdoncoffee.com.au/wp-content/uploads/2020/06/Website-Privacy-Policy-.pdf">privacy policy</a></label>
--- src/components/elements/MapPopUp/MapPopUp.jsx
+++ src/components/elements/MapPopUp/MapPopUp.jsx
@@ -4,12 +4,14 @@ import './MapPopUp.scss'
 
 export default class MapPopUp extends Component {
 
+  // ReactHtmlParser - парсим текст из state(Origins) в html
   render() {
     let { countryClick, country, closePopUp, showPopUp } = this.props
-    let styleS = { opacity: `${showPopUp ? "1" : "0"}`, zIndex: `${showPopUp ? "1" : "-1"}` }
+    let styleWrapper = { opacity: `${showPopUp ? "1" : "0"}`, zIndex: `${showPopUp ? "20" : "-1"}` }
+    let stylePop = { opacity: `${showPopUp ? "1" : "0"}`, zIndex: `${showPopUp ? "200" : "-1"}` }
     return (
-      <div className="wrapper" id="wrapper" style={styleS}>
-        <div className="map-popup" id="popup" style={styleS} >
+      <div className="wrapper" id="wrapper-popup" style={styleWrapper} >
+        <div className="map-popup" id="popup" style={stylePop} >
           <button className="popup__close" onClick={closePopUp}>&times;</button>
           <div className="popup__block">
             <div className="popup__header"></div>
